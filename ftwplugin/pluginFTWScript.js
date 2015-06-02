314ad7bd9ac42088acfb2421825846feb863f49e
Graham Macphee <grahammacphee@googlemail.com>
2015-06-02 18:17:57 +0100

    More plugin fixes (file paths)

--- ftwplugin/pluginFTWScript.js
+++ ftwplugin/pluginFTWScript.js
@@ -39,7 +39,7 @@ $(document).ready(function($){
 			$("head").append(cssLink); 
 		};
 
-		loadCSS("http://me.gmph.co/spyplugin/pluginFTWStyle.css");
+		loadCSS("http://me.gmph.co/ftwplugin/pluginFTWStyle.css");
 
 		var url = $(location).attr('href'); 
 		var hash = $(location).attr('hash');
